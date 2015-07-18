5d82f18af8712f982f003a1218678d13c2d3898a
Kunal Kamath <kkamath101@gmail.com>
2015-07-18 06:45:29 -0700

    Tester in progress

--- bechdel_test.py
+++ bechdel_test.py
@@ -5,9 +5,11 @@ from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 from cStringIO import StringIO
+from datetime import datetime
 import string
 import subprocess
 import re
+
 
 
 def convert_pdf(path):
@@ -33,105 +35,98 @@ def convert_pdf(path):
 
 
 def find_names(in_string):
- #   content = [line.rstrip('\n') for line in open(filename)] <-- if files were func input
+    content = [line.rstrip('\n') for line in open(in_string)] #<-- if files were func input CHANGE THIS BACK FOR STRINGS, NOT FILES!!!!!!!!!!!!!!!!!
+    chars = set('1234567890$,!@#%^&*()?-.:')
     listy = []
     sub_listy = []
     script = ''
     count = 0
-    for line in in_string.splitlines():
-        if line.isupper() and line.isalpha():
-            if count > 0:
+    for line in content:
+    #for line in in_string.splitlines():
+        if line[:1] == '(':
+            continue
+        if line.isdigit() and len(line) == 1:
+            scene_numer = int.line
+        elif line.isupper() and not any((c in chars) for c in line):
+            count +=1
+            if count > 1:                               # MAKE SURE THIS WORKS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! (Was originally 4)
                 sub_listy.append(script)
                 listy.append(sub_listy)
             sub_listy = []
             script = ''
             sub_listy.append(line)
-            count += 1
         else:
-            script = script + line.translate(string.maketrans('',''), string.punctuation) + ' '
+            script = script + line.translate(string.maketrans('', ''), string.punctuation) + ' '
     return listy
 
 
 # male keywords
-man_cave = ["man", "men", "guy", "guys", "he", "him", "his"]
+man_cave = ["man", "men", "guy", "guys", "he", "him", "himself", "his", "boy", "boyfriend", "boys", "boyfriends", "gentlemen"]
 
 def isWoman(name):
     result = subprocess.check_output(["./name-genders", "gender-get_gender", name]).split()
+    if result[len(result) - 1] == "female'":
+        return True
+    return False
+
+def isMan(name):
+    result = subprocess.check_output(["./name-genders", "gender-get_gender", name]).split()
     if result[len(result) - 1] == "male'":
-        return False
-    return True
+        return True
+    return False
 
 def man_proof(lines):
     for line in lines:
         line = re.findall(r"[\w']+", line.lower())
         for word in line:
-            print word
+            # print word
             if word in man_cave:
-                print "male keyword detected"
+                # print word
+                # print "male keyword detected"
                 return False
-            if not isWoman(word):
-                print "male name detected"
+            if isMan(word):
+                # print word
+                # print "male name detected"
                 return False
     return True
 
 def bechdel_test(lines):
     women_count = 0
     women_lines = []
-    not_sexist = False
-
     for i in range(0, len(lines)):
         line = lines[i]
-        if isWoman(line[0]):
+        if isWoman(line[0]) or 'LADY' in line[0] or 'DIDO' in line[0]:
+            # print line[0]
             women_count += 1
             if women_count == 3:
-                not_sexist = True
                 for j in range(i-2, i+1):
                     women_lines.append(lines[j][1])
+                    # print women_lines
             if women_count > 3:
                 women_lines.append(line[1])
-
         else:
             women_count = 0
             if women_lines:
-                if not man_proof(women_lines):
-                    not_sexist = False
-                    print "weak. ur movie only passed the first 2 Bechdel tests"
-            del women_lines[:]
+                if man_proof(women_lines):
+                    print women_lines
+                    return "ur movie passes all 3 Bechdel tests! good job"
+                del women_lines[:]
+    return "weak. ur movie is probably sexist"
 
-        print "women count: %d" % women_count
-        print women_lines
-
-    if women_lines:
-        if not man_proof(women_lines):
-            not_sexist = False
-
-    if not_sexist:
-        print "ur movie passes all 3 Bechdel tests! good job"
-    else:
-        print "weak. ur movie only passes 2 Bechdel tests"
 
 
 ######################### TESTING #########################
 
-with open('test.txt', 'w') as out_file:
-    print >> out_file, find_names(convert_pdf('birdman_mini_script_book.pdf'))
+startTime = datetime.now()
+with open('screenplay2.txt', 'w') as out_file:
+    list = find_names(convert_pdf('belle.pdf'))
+    print >> out_file, bechdel_test(list)
+print datetime.now() - startTime
+
 
 '''
-print find_names('example.txt')
-
-with open('test.txt', 'w') as out_file:
-    print >> out_file, find_names('example.txt')
+##### When testing with a sample file ######
+lst = find_names('screenplay.txt')
+with open('trial.txt', 'w') as out_file:
+        print >> out_file, bechdel_test(lst)
 '''
-
-
-
-sample_lines = []
-sample_lines.append(["Annie", "Hello, how are you? I am not talking about men"])
-sample_lines.append(["Lauren", "Hey Annie, omg what are the odds I'm also not talking about Bob"])
-sample_lines.append(["Annie", "No fucking way. We are so great"])
-sample_lines.append(["Lauren", "Yeah ik right? This is awesome"])
-sample_lines.append(["Melissa", "Hi I'm here to ruin ur perfect man-free convo"])
-sample_lines.append(["Annie", "fuck"])
-
-bechdel_test(sample_lines)
-
