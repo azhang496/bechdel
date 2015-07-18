4c0c5a61bf737a3c70990c644b1c90f9b41eade8
Kunal Kamath <kkamath101@gmail.com>
2015-07-18 01:59:49 -0700

    PDF processing into list character lines

--- bechdel_test.py
+++ bechdel_test.py
@@ -1,7 +1,56 @@
 # Python program to tell you if a screenplay passes the Bechdel test
 
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from cStringIO import StringIO
+import string
 import subprocess
 import re
+
+
+def convert_pdf(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = file(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos = set()
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching,
+                                  check_extractable=True):
+        interpreter.process_page(page)
+    fp.close()
+    device.close()
+    str = retstr.getvalue()
+    retstr.close()
+    return str
+
+
+def find_names(in_string):
+ #   content = [line.rstrip('\n') for line in open(filename)] <-- if files were func input
+    listy = []
+    sub_listy = []
+    script = ''
+    count = 0
+    for line in in_string.splitlines():
+        if line.isupper() and line.isalpha():
+            if count > 0:
+                sub_listy.append(script)
+                listy.append(sub_listy)
+            sub_listy = []
+            script = ''
+            sub_listy.append(line)
+            count += 1
+        else:
+            script = script + line.translate(string.maketrans('',''), string.punctuation) + ' '
+    return listy
+
 
 # male keywords
 man_cave = ["man", "men", "guy", "guys", "he", "him", "his"]
@@ -60,6 +109,21 @@ def bechdel_test(lines):
         print "ur movie passes all 3 Bechdel tests! good job"
     else:
         print "weak. ur movie only passes 2 Bechdel tests"
+
+
+######################### TESTING #########################
+
+with open('test.txt', 'w') as out_file:
+    print >> out_file, find_names(convert_pdf('birdman_mini_script_book.pdf'))
+
+'''
+print find_names('example.txt')
+
+with open('test.txt', 'w') as out_file:
+    print >> out_file, find_names('example.txt')
+'''
+
+
 
 sample_lines = []
 sample_lines.append(["Annie", "Hello, how are you? I am not talking about men"])
